9ed1aa140f75c9bc07ef50f53462589ff19ceb44
michellecaner <michellecaner@gmail.com>
2022-09-04 23:57:00 -0500

    retrieve & list views for travel_tip, trip, trip_category & trip_tag

--- gogalapi/views/__init__.py
+++ gogalapi/views/__init__.py
@@ -3,3 +3,7 @@ from gogalapi.views.category import CategoryView
 from gogalapi.views.go_gal_user import GoGalUserView
 from gogalapi.views.my_go_gal import MyGoGalView
 from gogalapi.views.tag import TagView
+from gogalapi.views.travel_tip import TravelTipView
+from gogalapi.views.trip import TripView
+from gogalapi.views.trip_category import TripCategoryView
+from gogalapi.views.trip_tag import TripTagView
---
+++ gogalapi/views/travel_tip.py
@@ -0,0 +1,41 @@
+"""View module for handling requests about travel tips"""
+
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from gogalapi.models import TravelTip
+
+class TravelTipView(ViewSet):
+    """Go Gal travel tip view"""
+    
+    def retrieve(self, request, pk):
+        """Handle GET requests for SINGLE travel tip
+        
+        Returns:
+            Response -- JSON serialized travel tip
+        """
+        try:
+            travel_tip = TravelTip.objects.get(pk=pk)
+            serializer = TravelTipSerializer(travel_tip)
+            return Response(serializer.data)
+        except TravelTip.DoesNotExist as ex:
+            return Response({"message": ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+          
+    def list(self, request):
+        """Handle GET requests to get ALL travel tips
+        
+        Returns:
+            Response -- JSON serialized list of travel tips
+        """
+        travel_tips = TravelTip.objects.all()
+        serializer = TravelTipSerializer(travel_tips, many=True)
+        return Response(serializer.data)
+      
+class TravelTipSerializer(serializers.ModelSerializer):
+    """JSON serializer for travel tips"""
+    class Meta:
+        model = TravelTip
+        fields = ("id", "tip")
+
+      
